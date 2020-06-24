e0691cfec733a9d88e97c8974130148d274f2fad
Михаил <santa_dj@mail.ru>
2020-06-24 16:18:43 +0300

    почистил верстку

--- source/js/animation.js
+++ source/js/animation.js
@@ -5,7 +5,7 @@
 
   var onParallax = function (event) {
     this.querySelectorAll('.cloud-animation__image').forEach(cloud => {
-       var speed = cloud.getAttribute('data-speed');
+      var speed = cloud.getAttribute('data-speed');
 
       cloud.style.transform = 'translate(' + event.clientX * speed / CLOUD_RANGE + 'px,' + event.clientY * speed / CLOUD_RANGE + 'px)';
     });
@@ -13,4 +13,3 @@
 
   window.animation = onParallax;
 })();
-
